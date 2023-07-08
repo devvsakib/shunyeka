a4658b481a3467608cf21039b4de24a1adc53c36
DevvSakib <devvsakib@gmail.com>
2023-07-09 00:35:58 +0600

    user details page ui improved

--- frontend/src/components/UserDetails.jsx
+++ frontend/src/components/UserDetails.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { selectUsers } from '../store/userSlice';
+import Title from './Common/Title';
 
 const UserDetails = () => {
   const { userId } = useParams();
@@ -14,18 +15,20 @@ const UserDetails = () => {
 
   return (
     <div>
-      <h2>User Details</h2>
-      <div>
-        <strong>ID:</strong> {user._id}
-      </div>
-      <div>
-        <strong>Name:</strong> {user.name}
-      </div>
-      <div>
-        <strong>Email:</strong> {user.email}
-      </div>
-      <div>
-        <strong>Phone:</strong> {user.phone}
+      <Title text="User Details" />
+      <div className='grid justify-center mt-20 gap-2'>
+        <div>
+          <strong>Name:</strong> {user.name}
+        </div>
+        <div>
+          <strong>Email:</strong> {user.email}
+        </div>
+        <div>
+          <strong>Phone:</strong> {user.phone}
+        </div>
+        <div>
+          <strong>ID:</strong> {user._id}
+        </div>
       </div>
     </div>
   );
