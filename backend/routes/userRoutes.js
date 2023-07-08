eb94dddc7adba2b9f6cea3f83c6da662c5bee57f
DevvSakib <devvsakib@gmail.com>
2023-07-08 22:05:42 +0600

    get user by ID API defined

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -18,12 +18,27 @@ router.post('/', async (req, res) => {
     try {
         const newUser = req.body;
         const user = await new User(newUser).save();
-        
+
         res.status(201).json(user);
 
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+// Get user by ID
+router.get('/:user_id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.user_id);
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ error: 'User not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 
 export { router as userRoutes };
