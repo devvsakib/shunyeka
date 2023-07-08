4f275a304d30382c7ab3002880dcc8d614b10b42
DevvSakib <devvsakib@gmail.com>
2023-07-08 22:23:20 +0600

    user details page added

--- frontend/src/components/UserDetails.jsx
+++ frontend/src/components/UserDetails.jsx
@@ -1,7 +1,34 @@
-const UserDetails = () => {
-  return (
-    <div>UserDetails</div>
-  )
-}
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { selectUsers } from '../store/userSlice';
 
-export default UserDetails
+const UserDetails = () => {
+  const { userId } = useParams();
+  const users = useSelector(selectUsers);
+  const user = users.find((u) => u._id === userId);
+
+  if (!user) {
+    return <div>User not found</div>;
+  }
+
+  return (
+    <div>
+      <h2>User Details</h2>
+      <div>
+        <strong>ID:</strong> {user._id}
+      </div>
+      <div>
+        <strong>Name:</strong> {user.name}
+      </div>
+      <div>
+        <strong>Email:</strong> {user.email}
+      </div>
+      <div>
+        <strong>Phone:</strong> {user.phone}
+      </div>
+    </div>
+  );
+};
+
+export default UserDetails;
